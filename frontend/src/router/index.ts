6367b7c12f94cb76344037f3a84c4857947b1b5b
adadi <adadi@outlook.com>
2025-10-07 15:15:59 +0200

    Remove settings route from the router configuration

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,7 +3,6 @@ import { useSettingsStore } from '../stores/settings'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
-  { path: '/settings', name: 'settings', component: () => import('../views/SettingsView.vue') },
   { path: '/deposit', name: 'deposit', component: () => import('../views/DepositView.vue') },
   { path: '/withdraw', name: 'withdraw', component: () => import('../views/WithdrawView.vue') },
   { path: '/transactions', name: 'transactions', component: () => import('../views/TransactionsView.vue') },
